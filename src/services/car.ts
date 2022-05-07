ecf1ed2bd6494d726df42bc8b26f0993de0e73c9
Femi Omotayo <omotayooluwafemi66@gmail.com>
2022-05-07 17:40:35 +0100

    list of cars and car details

--- src/services/car.ts
+++ src/services/car.ts
@@ -5,4 +5,12 @@ export class CarService {
   static async create(createCarData: CarAdvertDto) {
     return await CarRepository.create(createCarData);
   }
+
+  static async getAll() {
+    return await CarRepository.findAll();
+  }
+
+  static async getCarDetails(id: string) {
+    return await CarRepository.findOne({ _id: id });
+  }
 }
