132a8968ee13d350130e5fa85164fe0255849621
Femi Omotayo <omotayooluwafemi66@gmail.com>
2022-05-11 19:38:51 +0100

    heroku push

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,5 @@ routes(app);
 const port = process.env.PORT || 5000;
 
 app.listen(5000, () =>
-  console.log(`App Started Successfully on port ${port} `)
+  console.log(`App Started Successfully on port ${port}`)
 );
